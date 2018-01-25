fc440ddc8791df1ccecf063e5c185d96baee0e52
Italo Morales F <i@italomoralesf.com>
2018-01-25 17:10:43 -0500

    extends (herencia)

--- extends (herencia)/index.php
+++ extends (herencia)/index.php
@@ -7,5 +7,7 @@ require 'Professor.php';
 $student = new Student('Luisa', 'Fantone');
 echo $student->greet();
 
+echo '<br>';
+
 $professor = new Professor('Italo', 'Morales');
 echo $professor->greet();
